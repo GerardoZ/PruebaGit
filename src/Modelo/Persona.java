16d718dcbc5f51b21ac7949bff16af08ff009d14
pxndx <pxndx@WIN-O06GCLKUQT6>
2016-01-31 00:13:53 -0600

    Se renombró paquete a Vista Se agregó paquete Controlador Se agregó clase Persona

--- src/Modelo/Persona.java
+++ src/Modelo/Persona.java
@@ -3,19 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pruebagit;
+package Modelo;
 
 /**
  *
  * @author pxndx
  */
-public class PruebaGit {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        System.out.println("Prueba");
-    }
+public class Persona {
     
 }
